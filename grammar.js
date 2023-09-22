166487fc1fcd4e8958c05abd2a281a8dabe1b963
Mike Leone <mleone896@gmail.com>
2023-09-22 06:22:19 -0400

    feat:  refactoring grammar
    refactoring grammar to add caveat parsing and make it easier to add new
    syntax

--- grammar.js
+++ grammar.js
@@ -1,4 +1,8 @@
-const UNICODE_LETTER = /\p{L}/, LETTER = choice(UNICODE_LETTER, ':', '/', '_', '=');
+const
+  UNICODE_LETTER = /\p{L}/,
+  LETTER = choice(UNICODE_LETTER, ':', '/', '_', '='),
+  newline = '\n',
+  terminator = choice(newline, ';', '\0');
 
 module.exports = grammar({
   name: 'authzed',
@@ -7,8 +11,20 @@ module.exports = grammar({
   word: ($) => $.identifier,
 
   rules: {
-    source_file: ($) => $.body,
-    body: ($) => repeat1(choice($.relation, $.permission, $.block)),
+    // source_file: ($) => $.body,
+    source_file: ($) => seq(
+      repeat(choice(
+        seq($._statement, terminator),
+      )),
+    ),
+
+
+    _statement: ($) => choice(
+      $.relation,
+      $.permission,
+      $.caveat,
+      $.block,
+    ),
 
     relation: ($) =>
       seq(
@@ -33,26 +49,77 @@ module.exports = grammar({
         $.block_end,
       ),
 
+    caveat: ($) =>
+      seq(
+        'caveat',
+        field('name', $.identifier),
+        field('type_parameters', $.parameters_list),
+        field('body', optional($.block)),
+      ),
+
+    parameters_list: ($) => seq(
+      '(',
+      optional(seq(
+        commaSep(choice($.parameter_declaration, $.variadic_parameter_declaration)),
+        optional(','),
+      )),
+      ')',
+    ),
+
+    parameter_declaration: ($) => prec.left(seq(
+      commaSep(field('name', $.identifier)),
+      field('type', $._type),
+    )),
+
+    _type: ($) => choice(
+      $._simple_type,
+      $.parenthesized_type,
+    ),
+
+    variadic_parameter_declaration: ($) => seq(
+      field('name', optional($.identifier)),
+      '...',
+      field('type', $._type),
+    ),
+
     perm_expression: ($) =>
       prec.right(repeat1(choice($.identifier, $.plus_literal, $.stabby))),
 
     rel_expression: ($) =>
       prec.right(repeat1(choice($.identifier, $.pipe_literal, $.hash_literal))),
 
-    relation_literal: ($) => 'relation',
-    permission_literal: ($) => 'permission',
-    definition_literal: ($) => 'definition',
 
-    plus_literal: ($) => '+',
-    pipe_literal: ($) => '|',
-    slash_literal: ($) => '/',
-    stabby: ($) => '->',
-    block_start: ($) => '{',
-    block_end: ($) => '}',
-    equal_literal: ($) => '=',
-    hash_literal: ($) => '#',
+    parenthesized_type: ($) => seq('(', $._type, ')'),
 
-    identifier: ($) =>
+
+    _simple_type: (_) => choice(
+      'any',
+      'int',
+      'uint',
+      'bool',
+      'string',
+      'double',
+      'bytes',
+      'duration',
+      'timestampt'),
+
+    relation_literal: (_) => 'relation',
+    permission_literal: (_) => 'permission',
+    definition_literal: (_) => 'definition',
+    caveat_literal: (_) => 'caveat',
+
+    plus_literal: (_) => '+',
+    pipe_literal: (_) => '|',
+    slash_literal: (_) => '/',
+    stabby: (_) => '->',
+    block_start: (_) => '{',
+    block_end: (_) => '}',
+    equal_literal: (_) => '=',
+    hash_literal: (_) => '#',
+    paren_open: (_) => '(',
+    paren_close: (_) => ')',
+
+    identifier: (_) =>
       token(seq(LETTER, repeat(choice(LETTER, UNICODE_LETTER)))),
 
     comment: ($) =>
@@ -63,3 +130,31 @@ module.exports = grammar({
     _whitespace: ($) => token(/\s/),
   },
 });
+
+
+/**
+ * Creates a rule to match one or more of the rules separated by a comma
+ *
+ * @param {Rule} rule
+ *
+ * @return {SeqRule}
+ *
+ */
+function commaSep1(rule) {
+  return seq(rule, repeat(seq(',', rule)));
+}
+
+/**
+ * Creates a rule to optionally match one or more of the rules separated by a comma
+ *
+ * @param {Rule} rule
+ *
+ * @return {ChoiceRule}
+ *
+ */
+function commaSep(rule) {
+  return optional(commaSep1(rule));
+}
+
+
+// body: ($) => repeat1(choice($.relation, $.permission, $.block, $.caveat)),
